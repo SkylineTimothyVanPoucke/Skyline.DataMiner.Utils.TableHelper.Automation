3cdb6a7157104c053511421ce2c7463349b071b8
TimothyVanPoucke <timothy.van.poucke@skyline.be>
2023-09-19 11:53:34 +0200

    Add project files.

---
+++ Utils.TableHelper.Automation/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿namespace Skyline.DataMiner.Utils.TableHelper.Automation
+{
+    using DataMiner.Automation;
+    using Net.Messages;
+    using System;
+
+    /// <summary>
+    /// Helper functions of the MCR Swimlane solution.
+    /// </summary>
+    public static class ExtensionMethods
+    {
+        public static object[] GetTableColumns(this IEngine engine, int dmaId, int elementId, int tablePid, int[] columnPids)
+        {
+            return TableRetrieval.GetTableColumns(engine, dmaId, elementId, tablePid, columnPids);
+        }
+
+        public static object[] GetTableColumns(this Element element, IEngine engine, int tablePid, int[] columnPids)
+        {
+            return TableRetrieval.GetTableColumns(engine, element.DmaId, element.ElementId, tablePid, columnPids);
+        }
+    }
+}
---
+++ Utils.TableHelper.Automation/TableRetrieval/TableRetrieval.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Skyline.DataMiner.Automation;
+using Skyline.DataMiner.Net.Messages;
+
+namespace Skyline.DataMiner.Utils.TableHelper.Automation
+{
+    internal static class TableRetrieval
+    {
+        public static object[] GetTableColumns(IEngine engine, int dmaId, int elementId, int tablePid, int[] columnPids)
+        {
+            string[] filter = TableRetrieval.GetFilterString(columnPids);
+            var message = new GetPartialTableMessage(dmaId, elementId, tablePid, filter);
+            var requestedTable = (ParameterChangeEventMessage)engine.SendSLNetSingleResponseMessage(message);
+
+            if (requestedTable.NewValue.ArrayValue == null || requestedTable.NewValue.ArrayValue[0].ArrayValue == null)
+            {
+                return null;
+            }
+
+            int nrOfColumns = requestedTable.NewValue.ArrayValue.Length - 1; // drop the first column because the call will always return the PK, even if you don't ask for it.
+            int nrOfRows = requestedTable.NewValue.ArrayValue[0].ArrayValue.Length;
+
+            object[] columns = new object[nrOfColumns];
+
+            // start from 1 to skip the first column because the call will always return the PK, even if you don't ask for it.
+            for (int i = 1; i < nrOfColumns; i++)
+            {
+                object[] tempColumns = new object[nrOfRows];
+                for (int j = 0; j < nrOfRows; j++)
+                {
+                    tempColumns[j] = requestedTable.NewValue.ArrayValue[i].ArrayValue[j].ArrayValue[0].InteropValue;
+                }
+
+                columns[i] = tempColumns;
+            }
+
+            return columns;
+        }
+
+        public static string[] GetFilterString(int[] columnPids)
+        {
+            return new string[] { "columns=" + String.Join(",", columnPids) };
+        }
+    }
+}
